70ed557bb410ac9d5bdf6fec7fc307670ef524b8
Pramod Shekhar <120884568+Prabachi@users.noreply.github.com>
2023-03-21 22:00:14 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,15 +1,16 @@
 provider "aws"{
-region = "ap-southeast-1"
+	region = "ap-south-1"
 }
 
-resource "aws_instance" "AWSInstance" {
-     ami = "ami-0319ac76374b9fe74"
-	 instance_type = "t2.micro"
-	 key_name = "devopsmss"
-	 security_groups = ["launch-wizard-2"]
-	 tags = {
-	 Name = "tomcatservers"
-	 }
+resource "aws_instance" "AWSInstance"{
+	ami = "ami-0e07dcaca348a0e68"
+	instance_type = "t2.micro"
+	key_name = "pemfilemumbai"
+	security_groups = ["launch-wizard-2"]
+	tag = {
+	Name = "tomcatservers"
+	}
 }
+
 
 
